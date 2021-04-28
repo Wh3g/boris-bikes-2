e94c01f35117fbb281b01e59fef64950d2b036f9
willoughby_hood <willoughbyhood29@googlemail.com>
2021-04-28 18:51:32 +0100

    challenge 16

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,32 @@
 
 class DockingStation
-  attr_reader :docked_bike
+  DEFAULT_CAPACITY = 20
+  attr_reader :docked_bikes
+
+  def initialize
+    @docked_bikes = []
+  end
+
   def dock_bike(bike)
-    @docked_bike = bike
+    if full?
+      fail "Docking station is full"
+    end
+    @docked_bikes << bike
   end
 
   def release_bike
-    unless @docked_bike
+    if empty?
       fail "No bikes available"
     end
-    @docked_bike
+    @docked_bikes.pop
+  end
+
+  private
+  def full?
+    @docked_bikes.count >= DEFAULT_CAPACITY
+  end
+  
+  def empty?
+    @docked_bikes.count == 0 
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,19 @@
 require "docking_station"
 describe DockingStation do
-  it "responds to dock_bike" do
-    bike = Bike.new
-    expect(subject.dock_bike(bike)).to eq bike
-  end
   it "returns docked bikes" do
     bike = Bike.new
     subject.dock_bike(bike)
-    expect(subject.docked_bike).to eq bike
+    expect(subject.docked_bikes).to eq [bike]
+  end
+  describe "#dock_bike" do
+    it "responds to dock_bike" do
+      bike = Bike.new
+      expect(subject.dock_bike(bike)).to eq [bike]
+    end
+    it "Errors when docked_bikes are at capacity" do
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock_bike(Bike.new)}
+      expect {subject.dock_bike(Bike.new)}.to raise_error "Docking station is full"
+    end
   end
   describe "#release_bike" do
     it "releases a bike" do
@@ -15,7 +21,7 @@ describe DockingStation do
       subject.dock_bike(bike)
       expect(subject.release_bike).to eq bike
     end
-    it "Errors when docked_bike is empty" do
+    it "Errors when docked_bikes are empty" do
       expect {subject.release_bike}.to raise_error "No bikes available"
     end
   end
