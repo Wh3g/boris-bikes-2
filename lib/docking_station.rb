e0367adf8b145b90931a3f26d1d969a148b98a05
willoughby_hood <willoughbyhood29@googlemail.com>
2021-04-28 16:03:02 +0100

    challenge 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,11 @@
-class DockingStation
-  def release_bike
 
+class DockingStation
+  attr_reader :docked_bike
+  def dock_bike(bike)
+    @docked_bike = bike
+  end
+
+  def release_bike
+    Bike.new
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,5 +4,14 @@ describe DockingStation do
     dock = DockingStation.new
     expect(dock).to respond_to(:release_bike)
   end
+  it "responds to dock_bike" do
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to eq bike
+  end
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.docked_bike).to eq bike
+  end
 end
 
